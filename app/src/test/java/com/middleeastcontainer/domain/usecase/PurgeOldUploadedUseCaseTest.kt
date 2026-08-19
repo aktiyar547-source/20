af25cf9e3d9afb2a88448ff82c7bb63fd7542876
aktiyar547-source <aktiyar547@gmail.com>
2026-08-19 05:01:08 +0400

    Fix: Remove duplicate search() method in PurgeOldUploadedUseCaseTest RecordingRepo

--- app/src/test/java/com/middleeastcontainer/domain/usecase/PurgeOldUploadedUseCaseTest.kt
+++ app/src/test/java/com/middleeastcontainer/domain/usecase/PurgeOldUploadedUseCaseTest.kt
@@ -27,7 +27,6 @@ class PurgeOldUploadedUseCaseTest {
         override suspend fun create(name: String, type: String) {}
         override suspend fun updateType(name: String, type: String) {}
         override suspend fun delete(name: String) {}
-        override suspend fun search(query: String): Flow<List<Container>> = emptyFlow()
         override suspend fun purgeUploadedBefore(cutoffDate: String) { purgedCutoff = cutoffDate }
     }
 
